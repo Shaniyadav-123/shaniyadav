ff428ce1cb2d0e39a2b291e10564624c958231f5
Shaniyadav-123 <153608859+Shaniyadav-123@users.noreply.github.com>
2024-09-02 19:50:50 +0530

    g

---
+++ src/app/img/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ImagCompressor from './ImagCompressor'
+function page() {
+  return (
+    <div>
+     <ImagCompressor/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/imgFormet/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Imgformet from './Imgformet'
+
+function page() {
+  return (
+    <div>
+      <Imgformet/>
+    </div>
+  )
+}
+
+export default page
